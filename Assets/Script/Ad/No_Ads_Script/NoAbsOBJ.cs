6b788b251035c4edc0691c246b5d6fb93115b9e5
CTO <nhchoi98@gmail.com>
2022-03-09 00:54:08 +0900

    광고 쿼터 부족 연출 수정 (03.09)

--- Assets/Script/Ad/No_Ads_Script/NoAbsOBJ.cs
+++ Assets/Script/Ad/No_Ads_Script/NoAbsOBJ.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class NoAbsOBJ : MonoBehaviour
 {
-    
+    public Sequence hitSequence;
+    public Image image;
+    public Text text;
     void Start()
     {
         StartCoroutine(No_Ads_Info());
@@ -12,11 +15,15 @@ public class NoAbsOBJ : MonoBehaviour
 
     IEnumerator No_Ads_Info()
     {
-        Vector2 Target_Pos = new Vector2(0f, 400f);
-        transform.DOMove(Target_Pos, 2.5f)
-            .SetEase(Ease.OutQuint);
-        
-        Destroy(this.gameObject);
+        Vector2 Target_Pos = new Vector2(0f, 700f);
+        hitSequence = DOTween.Sequence()
+            .Append(transform.DOMove(Target_Pos, 1.8f)
+                .SetEase(Ease.OutQuint)
+                .OnComplete(() => Destroy(this.gameObject)))
+            .Insert(1.5f, DOTween.ToAlpha(() => image.color, alpha => image.color = alpha, 0f, 0.25f))
+            .Insert(1.5f, DOTween.ToAlpha(() => text.color, alpha => text.color = alpha, 0f, 0.25f));
+
+
         yield return null;
 
     }
